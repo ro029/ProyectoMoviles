e370a1ea8bf3578d9e14e3c320fa9ee24101c0cc
ro029 <20131797@aloe.ulima.edu.pe>
2016-07-06 09:39:21 -0500

    4

---
+++ app/src/main/java/pe/edu/ulima/myapplication/PromocionActivity.java
@@ -0,0 +1,13 @@
+package pe.edu.ulima.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PromocionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_promocion);
+    }
+}
