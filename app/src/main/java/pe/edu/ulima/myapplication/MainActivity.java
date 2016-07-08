23e60172eb3f3800da637c78e719c75a2107c062
DilerxD <20131349@aloe.ulima.edu.pe>
2016-07-08 13:53:24 -0500

    Facebook

--- app/src/main/java/pe/edu/ulima/myapplication/MainActivity.java
+++ app/src/main/java/pe/edu/ulima/myapplication/MainActivity.java
@@ -2,14 +2,44 @@ package pe.edu.ulima.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.facebook.AccessToken;
+import com.facebook.AccessTokenTracker;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookSdk;
+import com.facebook.login.widget.LoginButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    /* *************************************
+     *              FACEBOOK               *
+     ***************************************/
+    /* The login button for Facebook */
+    private LoginButton mFacebookLoginButton;
+    /* The callback manager for Facebook */
+    private CallbackManager mFacebookCallbackManager;
+    /* Used to track user logging in/out off Facebook */
+    private AccessTokenTracker mFacebookAccessTokenTracker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        /*mFacebookCallbackManager = CallbackManager.Factory.create();
+        mFacebookLoginButton = (LoginButton) findViewById(R.id.login_with_facebook);
+        mFacebookAccessTokenTracker = new AccessTokenTracker() {
+            @Override
+            protected void onCurrentAccessTokenChanged(AccessToken oldAccessToken, AccessToken currentAccessToken) {
+                Log.i(TAG, "Facebook.AccessTokenTracker.OnCurrentAccessTokenChanged");
+                MainActivity.this.onFacebookAccessTokenChange(currentAccessToken);*/
+            }
+
+        };
+
     }
+
+
 
 
 }
