11acf92097681fda8d3d22b78d80a34f56be7bf4
ro029 <20131797@aloe.ulima.edu.pe>
2016-07-15 01:53:01 -0500

    sugerencias bonis :heart:

--- app/src/main/java/pe/edu/ulima/myapplication/Fragments/SugerenciasActivity.java
+++ app/src/main/java/pe/edu/ulima/myapplication/Fragments/SugerenciasActivity.java
@@ -3,12 +3,25 @@ package pe.edu.ulima.myapplication.Fragments;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListView;
+
+import com.google.firebase.database.DatabaseReference;
+
+import pe.edu.ulima.myapplication.R;
+import pe.edu.ulima.myapplication.adapter.Adapter;
 
 public class SugerenciasActivity extends Fragment {
- /*
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sugerencias);
-    }*/
+    //ArrayList<Cultura> cultura=new ArrayList<>();
+    private static DatabaseReference ref;
+    Adapter adapter;
+    ListView lista;
+
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.activity_sugerencias,container,false);
+
+        return v;
+    }
 }
